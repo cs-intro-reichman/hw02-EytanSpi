caf22e982bc122b13d25d418193258e630b594ea
Eytan Spialter <eytan.spialter@runi.ac.il>
2023-12-26 15:36:45 +0200

    Divisors Code

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,12 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int x = Integer.parseInt(args[0]);
+		for (int i = 1; i <= x/2; i++)  {  //check all numbers up to i/2 for efficiency
+			if (x % i == 0) {
+				System.out.println(i);
+			}
+		}
+		System.out.println(x);
 	}
 }

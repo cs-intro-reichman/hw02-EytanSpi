4e3f09d511e120e25b1f210c6f32f5490adafe06
Eytan Spialter <eytan.spialter@runi.ac.il>
2023-12-26 16:34:55 +0200

    InOrder - Code

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,27 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		int n = (int) (Math.random() * 10);
+		int old = -1; // so as that n will always be greater than old at the start.
+		while (old <= n) {
+			// Generates and prints random number in [0,10).
+			System.out.print(n + " ");
+			old = n;
+			n = (int) (Math.random() * 10);
+		}
+
+/* //this is code that i wrote for myself to test the Do-While Loop.
+	int n = (int) (Math.random() * 10);
+		int old = -1; // so as that n will always be greater than old at the start.
+		do {
+			// Generates and prints random number in [0,10).
+			System.out.print(n + " ");
+			old = n;
+			n = (int) (Math.random() * 10);
+		} while (old <= n);
+*/
+
+
 	}
+
 }

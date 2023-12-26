900d6412dd4eb50fdc24c26ec12742e7f8b11203
Eytan Spialter <eytan.spialter@runi.ac.il>
2023-12-26 19:35:33 +0200

    Wrote code for Perfect

--- InOrder.java
+++ InOrder.java
@@ -5,7 +5,7 @@
 public class InOrder {
 	public static void main (String[] args) {
 		int n = (int) (Math.random() * 10);
-		int old = -1; // so as that n will always be greater than old at the start.
+		int old = 0; // so as that 'n' will always be greater than or equal to 'old' at the start.
 		while (old <= n) {
 			// Generates and prints random number in [0,10).
 			System.out.print(n + " ");
@@ -13,9 +13,10 @@ public class InOrder {
 			n = (int) (Math.random() * 10);
 		}
 
-/* //this is code that i wrote for myself to test the Do-While Loop.
+/* 
+//this is code that i wrote for myself to test the Do-While Loop.
 	int n = (int) (Math.random() * 10);
-		int old = -1; // so as that n will always be greater than old at the start.
+	int old = 0; //  so as that 'n' will always be greater than or equal to 'old' at the start.
 		do {
 			// Generates and prints random number in [0,10).
 			System.out.print(n + " ");
@@ -23,7 +24,6 @@ public class InOrder {
 			n = (int) (Math.random() * 10);
 		} while (old <= n);
 */
-
 
 	}
 
--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,21 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+		int sum = 1; //we will be skipping the number one in the loop (1 is not perfect and is a divisor for every number)
+		String strPerfect = n + " is a perfect number since " + n + " = 1";
+		for (int i = 2; i <= n/2 && sum <= n; i++) { //check all numbers up to i/2, with option to stop if sum is greater than n - for optimization
+			if (n % i == 0) {
+				sum += i;
+				strPerfect = strPerfect + " + " + i;
+			}
+		}
+		if (sum != n) {
+			System.out.println(n + " is not a perfect number");
+		}
+		else {
+			System.out.println(strPerfect);
+		}
+
 	}
 }

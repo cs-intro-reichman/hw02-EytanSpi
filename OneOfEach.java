580633a3a698de8bf879eaac5c32ca02f0c74459
Eytan Spialter <eytan.spialter@runi.ac.il>
2023-12-27 12:40:27 +0200

    wrote code for OneOfEach

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,25 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		boolean bothGenders = false;
+		int firstGen = (int) (Math.random()*2); // 0-boy 1-girl
+		int count = 1;
+		if (firstGen == 0) {
+			System.out.print("b");
+		} else {
+			System.out.print("g");
+		}
+		while (!bothGenders) {
+			int gen = (int) (Math.random()*2); // 0-boy 1-girl
+			count++;
+			bothGenders = (firstGen != gen);
+			if (gen == 0) {
+				System.out.print(" b");
+			} else {
+				System.out.print(" g");
+			}
+		}
+		System.out.println();
+		System.out.println("You made it... and you now have " + count + " children.");
 	}
 }
